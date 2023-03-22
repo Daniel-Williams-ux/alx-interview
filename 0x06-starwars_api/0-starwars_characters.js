d34aff644cfa12e2b22b26c81143e09a2093cd2e
Daniel Williams <williamsoligodaniel@gmail.com>
2023-03-22 07:49:38 +0100

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,47 +2,26 @@
 
 const request = require('request');
 
-const movieId = process.argv[2];
-if (!movieId || isNaN(parseInt(movieId))) {
-  console.error('Invalid movie ID. Please provide a valid integer.');
-  process.exit(1);
-}
+const req = (arr, i) => {
+  if (i === arr.length) return;
+  request(arr[i], (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(JSON.parse(body).name);
+      req(arr, i + 1);
+    }
+  });
+};
 
-const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error retrieving film:', error.message);
-    process.exit(1);
+request(
+  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
+  (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      const chars = JSON.parse(body).characters;
+      req(chars, 0);
+    }
   }
-  if (response.statusCode !== 200) {
-    console.error(`Error retrieving film: ${response.statusCode} ${response.statusMessage}`);
-    process.exit(1);
-  }
-
-  const film = JSON.parse(body);
-  const characters = film.characters;
-  if (characters.length === 0) {
-    console.log('No characters found for this film.');
-    process.exit(0);
-  }
-
-  const characterUrls = characters.map((url) => url.replace('http', 'https'));
-  const characterNames = [];
-  let numResponses = 0;
-  for (let i = 0; i < characterUrls.length; i++) {
-    request(characterUrls[i], (error, response, body) => {
-      if (error) {
-        console.error(`Error retrieving character ${i+1}: ${error.message}`);
-      } else if (response.statusCode !== 200) {
-        console.error(`Error retrieving character ${i+1}: ${response.statusCode} ${response.statusMessage}`);
-      } else {
-        const character = JSON.parse(body);
-        characterNames[i] = character.name;
-      }
-      numResponses++;
-      if (numResponses === characterUrls.length) {
-        console.log(characterNames.join('\n'));
-      }
-    });
-  }
-});
+);
